63cf7ca597f8a6ada92464a6228a6db93af026e1
Choi-Jongin <ekejaqutmqk@gmail.com>
2021-05-18 14:22:11 +0900

    recover log file

---
+++ Gantt/gantt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include "../tgui.h"
+#include "../log.h"
+#include "excu.h"
+
+#define READ_BUFF_SIZE 256
+#define HEIGHT 30
+#define WIDTH 160
+
+TGUI * tgui;		//텍스트만으로 GUI처럼 화면에 표시하기 위한 구조체
+
+// │ {str} blank │
+
+
+
+int main(int argc, char *argv[] )
+{
+	tgui = newTGUI(HEIGHT,WIDTH);
+
+       
+
+
+
+    return 0;
+}
